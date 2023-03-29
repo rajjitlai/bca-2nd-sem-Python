e942c3d66683e587d18c14070a2d8ca302b5c364
Rajjit <2.0lairaj@gmail.com>
2023-03-29 06:49:11 +0530

    added extra

---
+++ Extra/floyds.py
@@ -0,0 +1,13 @@
+rows = int(input("Enter the number of rows: "))
+
+num = 1
+i = 1
+
+while i <= rows:
+    j = 1
+    while j <= i:
+        print(num, end=" ")
+        num += 1
+        j += 1
+    print()
+    i += 1
