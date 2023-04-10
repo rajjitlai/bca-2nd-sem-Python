1dd53b6ab350afeb1be7e73f1c6b31f3e7c481d8
Rajjit <2.0lairaj@gmail.com>
2023-04-10 07:04:43 +0530

    changed star.py

--- 2-printPatterns/star.py
+++ 2-printPatterns/star.py
@@ -1,33 +1,21 @@
 # Python program to print the pattern
 """
-          *
-          * * *
-          * * * * *
-          * * *
-          *
+                  *
+            *     *     *
+      *     *     *     *     *
 """
 
-# function definition
-def patternPrint(n):
-              for i in range(n):
-                    if i < n //2 + 1:
-                              for j in range(i*2 + 1):
-                                        print("*", end=" ")
-                    else:
-                              for j in range((n - i)*2 - 1):
-                                        print("*", end=" ")
-                    print("")
+def printPattern():
+      # rows = int(input("Enter the number of rows: "))   
+      rows = 4
+      k = 2 * rows - 2
+      
+      for i in range(0, rows):
+            for j in range(0, k):
+                  print(end=' ')
+            k = k - 2
+            for j in range(0, 2 * i - 1):
+                  print("*", end=" ")
+            print(" ")
 
-# function call
-patternPrint(5)
-
-
-
-# alternate
-"""
-print("*")
-print("* * *")
-print("* * * * *")
-print("* * *")
-print("*")
-"""
+printPattern()
