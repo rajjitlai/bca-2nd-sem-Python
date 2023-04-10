21bffdbcb4bbdca84a9e75a7f89386a9274594db
Rajjit <2.0lairaj@gmail.com>
2023-04-10 07:35:01 +0530

    changed ser2sum with func

--- 11-seriessum/ser2sum.py
+++ 11-seriessum/ser2sum.py
@@ -1,16 +1,33 @@
 # Python program to find the sum of the series 1 + x2 /2! + x4 /4! + x6 /6! + … xn /n!
 
-n = int(input("Enter the term: "))
-x = int(input("Enter value of x: "))
-k = 2
-sum = 1
+# n = int(input("Enter the term: "))
+# x = int(input("Enter value of x: "))
+# k = 2
+# sum = 1
 
-for i in range(2, n+1, 2):
-          fact = 1
-          for j in range(1, i+1):
-                    fact *= j
-          sum += ((x**i)/fact)
+# for i in range(2, n+1, 2):
+#           fact = 1
+#           for j in range(1, i+1):
+#                     fact *= j
+#           sum += ((x**i)/fact)
 
-print("Sum = ", sum)
+# print("Sum = ", sum)
 
 
+
+# using function
+
+def series_sum(n, x):
+          k = 2
+          sum = 1
+
+          for i in range(2, n+1, 2):
+                    fact = 1
+                    for j in range(1, i+1):
+                              fact *= j
+                    sum += ((x**i)/fact)
+
+          return sum
+
+result = series_sum(5, 2)
+print(result) 
