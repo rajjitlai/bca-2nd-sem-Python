659d886ac91288e121f79bcdae21e61e086df89a
Rajjit <2.0lairaj@gmail.com>
2023-04-17 19:59:07 +0530

    added freqLetter.py

---
+++ 18-freqLetter/freqLetter.py
@@ -0,0 +1,13 @@
+# a function that takes a sentence as input from the user and calculates the frequency of each letter and use a variable of dictionary type to maintain the count
+
+
+def freqLetter():
+          userInput = input("Enter a sentence: ")
+          frequency = {}
+          for letter in userInput:
+                    if letter.isalpha():
+                              letter = letter.lower()
+                              frequency[letter] = frequency.get(letter, 0) + 1
+          print(frequency)
+          
+freqLetter()
