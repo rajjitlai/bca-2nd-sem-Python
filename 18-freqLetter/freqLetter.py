0c03f9f80916937e31f5eeaab31e299ff4059546
Rajjit <2.0lairaj@gmail.com>
2023-04-22 07:14:26 +0530

    changed freqLetter.py

--- 18-freqLetter/freqLetter.py
+++ 18-freqLetter/freqLetter.py
@@ -1,6 +1,6 @@
 # Python function that takes a sentence as input from the user and calculates the frequency of each letter and use a variable of dictionary type to maintain the count
 
-
+"""
 def freqLetter():
           userInput = input("Enter a sentence: ")
           frequency = {}
@@ -11,3 +11,23 @@ def freqLetter():
           print(frequency)
           
 freqLetter()
+"""
+
+# From the code taught in the class
+my_sentence = []
+sentence = input("Enter a sentence: ")
+freq = {}
+my_sentence = sentence.split()
+print("The words in the sentence are: ", my_sentence)
+for word in my_sentence:
+    if word in freq:
+        freq[word] += 1
+    else:
+        freq[word] = 1
+
+check_word = input("Enter a word to check its frequency in the sentence: ")
+if check_word in freq:
+    print(check_word, "has a frequency of", freq[check_word], "in the given sentence")
+else:
+    print(check_word, "not found in the sentence")
+
